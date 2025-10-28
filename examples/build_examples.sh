afdfd3f73bdc77ea4c9065471a2722dabdb8ff77
Eser KUBALI <eserlxl@gmail.com>
2025-10-29 00:33:59 +0300

    Add GPL-3.0-or-later license header to all source files, headers, scripts, and CMakeLists.txt files

--- examples/build_examples.sh
+++ examples/build_examples.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Copyright © 2025 Eser KUBALI <lxldev.contact@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This file is part of LLMEngine and is licensed under
+# the GNU General Public License v3.0 or later.
+# See the LICENSE file in the project root for details.
+
 # LLMEngine Examples Build Script
 # This script builds all example applications
 
